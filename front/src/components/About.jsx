f3884cf978609281b04731f9a9dd61c2fb772464
Rumman Sheikh <rummansheikh2004@gmail.com>
2025-07-11 08:22:14 +0600

    Update About.jsx

--- front/src/components/About.jsx
+++ front/src/components/About.jsx
@@ -54,9 +54,9 @@ function About() {
                     <FaXTwitter className=" text-white size-6" />
                   </button>
                 </a>
-                <a href="">
+                <a href="https://letsbonk.fun/">
                   <button className=" p-2 bg-[#00B600] rounded-md border border-black shadow-[1.5px_3.5px_0px_0px_#000000] text-white">
-                    PUMP
+                    BONK
                   </button>
                 </a>
               </div>
